e8dc9dda58e7bc56b2e441237fa91c85146bc7c5
恼人转转愁生 <lianguangran@gmail.com>
2020-05-02 04:52:56 +0800

    add org dir for org-mode.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,10 +3,10 @@
 	 ("C-c a" . 'org-agenda)
 	 ("C-c r" . 'org-capture))
   :config
-  (setq org-agenda-files '("~/.emacs.d/"))
+  (setq org-agenda-files '("~/.emacs.d/org/"))
   (setq org-src-fontify-natively t)
   (setq org-capture-templates
-	'(("t" "Todo" entry (file+headline "~/.emacs.d/schedule.org" "Schedule")
+	'(("t" "Todo" entry (file+headline "~/.emacs.d/org/schedule.org" "Schedule")
 	   "* TODO [#B] %?\r\n %i\r\n"
 	   :empty-lines 1))))
 
